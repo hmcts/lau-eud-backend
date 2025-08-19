e00caf4981c7f9622b29301cc7e62deed4258765
Priyanka Verma <priyanka.verma@hmcts.net>
2025-08-19 13:20:28 +0100

    LAU-1224 Database configuration using terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,129 @@
 provider "azurerm" {
   features {}
 }
+
+provider "azurerm" {
+  subscription_id            = var.aks_subscription_id
+  skip_provider_registration = "true"
+  features {}
+  alias = "postgres_network"
+
+}
+
+locals {
+  db_connection_options = "?sslmode=require"
+  vault_name            = "${var.product}-${var.env}"
+  asp_name              = "${var.product}-${var.env}"
+  env                   = var.env
+  db_server_name        = "${var.product}-${var.component}-flexible"
+}
+
+data "azurerm_key_vault" "key_vault" {
+  name                = local.vault_name
+  resource_group_name = local.vault_name
+}
+
+module "lau-eud-db-flexible" {
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  env           = var.env
+  subnet_suffix = var.subnet_suffix
+
+  product       = var.product
+  component     = var.component
+  business_area = "cft"
+  name          = local.db_server_name
+
+  common_tags = var.common_tags
+
+  pgsql_storage_mb          = var.pgsql_storage_mb
+  pgsql_delegated_subnet_id = var.pgsql_delegated_subnet_id
+
+  auto_grow_enabled = true
+
+  pgsql_admin_username = "lauadmin"
+  pgsql_version        = "15"
+
+  # Setup Access Reader db user
+  force_user_permissions_trigger = "1"
+
+  pgsql_databases = [
+    {
+      name : var.lau_eud_db_name
+    }
+  ]
+
+  pgsql_server_configuration = [
+    {
+      name  = "azure.extensions"
+      value = "pg_stat_statements,pg_buffercache,pgcrypto,hypopg"
+    }
+  ]
+
+  admin_user_object_id = var.jenkins_AAD_objectId
+
+  action_group_name          = join("-", [var.db_monitor_action_group_name, local.db_server_name, var.env])
+  email_address_key          = var.db_alert_email_address_key
+  email_address_key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+////////////////////////////////
+// Populate Vault with DB info
+////////////////////////////////
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "${var.component}-POSTGRES-USER"
+  value        = module.lau-case-db-flexible.username
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "${var.component}-POSTGRES-PASS"
+  value        = module.lau-case-db-flexible.password
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "${var.component}-POSTGRES-HOST"
+  value        = module.lau-case-db-flexible.fqdn
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "${var.component}-POSTGRES-PORT"
+  value        = var.postgresql_flexible_server_port
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "${var.component}-POSTGRES-DATABASE"
+  value        = var.lau_case_db_name
+}
+
+# Copy postgres password for flyway migration
+resource "azurerm_key_vault_secret" "flyway_password" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "eud-flyway-password"
+  value        = module.lau-case-db-flexible.password
+}
+
+resource "azurerm_key_vault_secret" "lau_case_db_user" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "eud-backend-app-db-user-flexible"
+  value        = "lauuser"
+}
+
+resource "azurerm_key_vault_secret" "case_request_key" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "eud-backend-encryption-key"
+  value        = random_password.password.result
+}
+
+resource "random_password" "password" {
+  length           = 32
+  override_special = "()-_"
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,16 +1,90 @@
-variable "product" {}
+variable "product" {
+  default = "lau"
+}
 
-variable "component" {}
+variable "component" {
+  default = "eud-backend"
+}
 
 variable "location" {
   default = "UK South"
 }
 
-variable "env" {}
+variable "location_db" {
+  default = "UK South"
+}
 
-variable "subscription" {}
+variable "env" {
+}
+
 
 variable "common_tags" {
   type = map(string)
+}
+
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "subscription" {
+}
+
+variable "common_tags" {
+  type = map(string)
+}
+
+variable "appinsights_location" {
+  default     = "West Europe"
+  description = "Location for Application Insights"
+}
+
+variable "appinsights_instrumentation_key" {
+  description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
+  default     = ""
+}
+
+variable "lau_eud_db_name" {
+  description = "Name of database to use"
+  default     = "lau_eud"
+}
+
+variable "aks_subscription_id" {}
+
+variable "postgresql_flexible_server_port" {
+  default = "5432"
+}
+
+variable "pgsql_storage_mb" {
+  type        = number
+  description = "Flexible Postgres DB size in mb"
+  default     = 131072
+}
+
+variable "subnet_suffix" {
+  default     = null
+  type        = string
+  description = "Suffix to append to the subnet name, the originally created one used by this module is full in a number of environments."
+}
+
+variable "db_monitor_action_group_name" {
+  description = "The name of the Action Group to create."
+  type        = string
+  default     = "db_monitor_ag"
+}
+
+variable "db_alert_email_address_key" {
+  description = "Email address key in azure Key Vault."
+  type        = string
+  default     = ""
+}
+
+variable "pgsql_delegated_subnet_id" {
+  description = "The subnet to delegate to the PostgreSQL server"
+  type        = string
+  default     = ""
 }
 
