1d7aa5c532b3520ebb63fca5279744be2ab708d1
Priyanka Verma <priyanka.verma@hmcts.net>
2025-10-09 08:19:04 +0100

    functional test refactor

--- src/functionalTest/java/uk/gov/hmcts/reform/laubackend/eud/serenityfunctionaltests/model/UserDataResponseVO.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/laubackend/eud/serenityfunctionaltests/model/UserDataResponseVO.java
@@ -2,53 +2,30 @@ package uk.gov.hmcts.reform.laubackend.eud.serenityfunctionaltests.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.annotation.Generated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-
-@Generated("jsonschema2pojo")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDataResponseVO {
+
+    @JsonProperty("userId")
+    private String userId;
+
+    @JsonProperty("email")
+    private String email;
 
     @JsonProperty("roles")
     private List<String> roles;
-    @JsonProperty("userId")
-    private String userId;
-    @JsonProperty("email")
-    private String email;
+
     @JsonProperty("accountStatus")
     private String accountStatus;
+
     @JsonProperty("accountCreationDate")
     private String accountCreationDate;
-
-    @JsonProperty("roles")
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    @JsonProperty("roles")
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
-
-    @JsonProperty("userId")
-    public String getUserId() {
-        return userId;
-    }
-
-    @JsonProperty("userId")
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    @JsonProperty("email")
-    public String getEmail() {
-        return email;
-    }
-
-    @JsonProperty("email")
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
